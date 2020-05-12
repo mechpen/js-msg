2da758cb765543c511d1b1d3bfed36cd74f0907e
Wenhua Zhao <whzhao@gmail.com>
2020-05-12 06:41:32 +0000

    fixed corner cases for path computing

--- cmd.js
+++ cmd.js
@@ -1,15 +1,14 @@
 #!/usr/bin/env node
 
-const argv = require("minimist")(process.argv.slice(2))
 const msg = require("./lib")
 
 async function main() {
   try {
-    msg.runConfig(argv)
-    await msg.genSite(argv._[0], argv._[1], argv)
+    let options = msg.checkArgs(process.argv.slice(2))
+    msg.runConfig(options)
+    await msg.genSite(options)
   } catch (e) {
     console.log(e)
-    console.log(e.stack)
   }
 }
 
